1db04cf61fbb89d23699ac16e471eb92f9695a97
AKToran <aktoran60@gmail.com>
2025-04-30 21:34:42 +0600

    login, register form and firebase, env

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router";
 
 const Login = () => {
   return (
-    <div>
-      login
+    <div className="card mx-auto mt-8 bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <h1 className="text-3xl font-bold text-center py-4">Login Now</h1>
+      <div className="card-body">
+        <form className="fieldset">
+          <label className="label">Email</label>
+          <input type="email" className="input" placeholder="Email" />
+          <label className="label">Password</label>
+          <input type="password" className="input" placeholder="Password" />
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn mt-4">Login</button>
+        </form>
+        <p>Don't have an account? <Link className="text-blue-600 hover:underline" to={'/register'}>Register</Link> </p>
+      </div>
     </div>
   );
 };
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router";
 
 const Register = () => {
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    console.log(name,email, password);
+  };
+
   return (
-    <div>
-      register
+    <div className="card mx-auto mt-8 bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <h1 className="text-3xl font-bold text-center py-4">Register Here</h1>
+      <div className="card-body">
+        <form onSubmit={handleRegister} className="fieldset">
+          <label className="label">Name</label>
+          <input type="text" name="name" className="input" placeholder="Name" />
+          <label className="label">Email</label>
+          <input type="email" name="email" className="input" placeholder="Email" />
+          <label className="label">Password</label>
+          <input type="password" name="password" className="input" placeholder="Password" />
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn mt-4">Register</button>
+        </form>
+        <p>
+          Already have an account?{" "}
+          <Link className="text-blue-600 link-hover" to={"/login"}>
+            Login
+          </Link>{" "}
+        </p>
+      </div>
     </div>
   );
 };
