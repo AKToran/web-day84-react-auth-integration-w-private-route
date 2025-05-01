e2a7b38f31aca0b403ba838a0116e38171020710
AKToran <aktoran60@gmail.com>
2025-05-01 23:15:27 +0600

    login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,22 +1,55 @@
-import React from "react";
+import React, { use } from "react";
 import { Link } from "react-router";
+import { AuthContext } from "../../Contexts/AuthContext";
 
 const Login = () => {
+  const { signinUser } = use(AuthContext);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    //login user
+    signinUser(email, password)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="card mx-auto mt-8 bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
       <h1 className="text-3xl font-bold text-center py-4">Login Now</h1>
       <div className="card-body">
-        <form className="fieldset">
+        <form onSubmit={handleLogin} className="fieldset">
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input
+            type="email"
+            name="email"
+            className="input"
+            placeholder="Email"
+          />
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <input
+            type="password"
+            name="password"
+            className="input"
+            placeholder="Password"
+          />
           <div>
             <a className="link link-hover">Forgot password?</a>
           </div>
           <button className="btn mt-4">Login</button>
         </form>
-        <p>Don't have an account? <Link className="text-blue-600 hover:underline" to={'/register'}>Register</Link> </p>
+        <p>
+          Don't have an account?{" "}
+          <Link className="text-blue-600 hover:underline" to={"/register"}>
+            Register
+          </Link>{" "}
+        </p>
       </div>
     </div>
   );
--- src/Contexts/AuthProvider.jsx
+++ src/Contexts/AuthProvider.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AuthContext } from './AuthContext';
 import { auth } from "../firebase.init"
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 
 const AuthProvider = ({children}) => {
@@ -10,8 +10,13 @@ const AuthProvider = ({children}) => {
     return createUserWithEmailAndPassword(auth, email, password);
   }
 
+  const signinUser = (email, password) =>{
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
   const userInfo = {
-    createUser
+    createUser,
+    signinUser
   }
 
   return (
